123344b2a43d4065025415bf6167fa67df7e8b53
Pratham443 <77573159+Pratham443@users.noreply.github.com>
2021-04-30 13:38:54 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDdohbmQDGasgma9WOlh2Y5oJ3kX4bmg_8",
+    authDomain: "kwitter-web-app-a0124.firebaseapp.com",
+    databaseURL: "https://kwitter-web-app-a0124-default-rtdb.firebaseio.com",
+    projectId: "kwitter-web-app-a0124",
+    storageBucket: "kwitter-web-app-a0124.appspot.com",
+    messagingSenderId: "229868928476",
+    appId: "1:229868928476:web:f670cbb3b97c30fab62319"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
